d8e2baedd5a65afbf2a3f243aa049b0040171722
AneetaAbhm <aneetaabhm02@gmail.com>
2023-03-16 13:10:40 +0530

    added Code

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$user = "root"; //test
+$password = ""; //mmaFkz6Og3fyKvD3J2fX
+$db = "fitness";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+// if ($conn) {
+//     echo "Success";
+// } else {
+//     echo "Fail";
+// }
+
+?>
+
